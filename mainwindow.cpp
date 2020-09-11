e94c86b0eee01dcb0a23de1f48aaf0b05f2861e7
sutefu23 <sutefu23@gmail.com>
2020-09-11 13:18:49 +0900

    設定バグ修正／新規で作らないモード追加／／６桁伝票追加／数字ではない時は新規作成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QtDebug>
+#include <time.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,7 +12,9 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     //設定ファイル読み込み
-    QSettings settings("order_directory.ini", QSettings::IniFormat);
+    QString path = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+    QSettings settings(path+"/order_directory.ini", QSettings::IniFormat);
+
     settings.setIniCodec(QTextCodec::codecForName("UTF-8"));
     settings.beginGroup("CONFIG");
     int windowX = settings.value("POSITION_X",50).toInt();
@@ -24,13 +28,15 @@ MainWindow::MainWindow(QWidget *parent)
 
     // 伝票テキストボックスにフォーカス
     ui->orderNumberEdit->setFocus();
+
 }
 
 //ウィンドウインスタンス破棄時
 MainWindow::~MainWindow()
 {
     //ウィンドウ位置保存
-    QSettings settings("order_directory.ini", QSettings::IniFormat);
+    QString path = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+    QSettings settings(path+"/order_directory.ini", QSettings::IniFormat);
     settings.setIniCodec(QTextCodec::codecForName("UTF-8"));
     settings.beginGroup("CONFIG");
     settings.setValue("POSITION_X",this->x());
@@ -68,32 +74,95 @@ void MainWindow::on_orderNumberEdit_returnPressed()
         return;
     }
 
-    //数値チェック
-    for (int i =0;i<orderNum.size();i++)
-    {
-       if (!orderNum[i].isDigit()){
-            QMessageBox::critical(this,tr("Error"),tr("伝票番号は全て数字である必要があります。"));
-            return;
+    //数値ではない場合、今月で新規作成して離脱
+    bool isDegit;
+    orderNum.toInt(&isDegit);
+    if(!isDegit){
+        time_t theTime = time(NULL);
+        struct tm *current = localtime(&theTime);
+        int year = current->tm_year + 1900;
+        int month = current->tm_mon + 1;
+
+        QString orderDir = QString::number(year) + "/" + QString::number(month) + "/" + orderNum;
+        QString createPath = root + '/' + orderDir;
+        QDir createDir(createPath);
+        if (!createDir.exists()){
+            if(!ui->createDirectoryCheck->isChecked()){//新規作成しないモードon
+                QMessageBox::warning(this,tr("Notice"),tr("伝票フォルダが見つかりません"));
+                return;
+            }
+            createDir.mkpath(".");
         }
-    }
-    if(orderNum.size() != 8 && orderNum.size() != 9){
-        QMessageBox::critical(this,tr("Error"),tr("伝票番号は８桁または９桁です。"));
+
+        //ディレクトリ開く
+        QDesktopServices::openUrl( QUrl::fromLocalFile ( createPath ));
+        //txtEditのクリア
+        ui->orderNumberEdit->clear();
         return;
     }
 
-    QString orderDir = "20" + orderNum.left(2) + "/" + orderNum.mid(2, 2) + "/" + orderNum.right(orderNum.length() - 4);
 
-    QString path = root + '/' + orderDir;
-
-    //存在しない場合は作成
-    QDir dir(path);
-    if (!dir.exists()){
-        dir.mkpath(".");
+    if(orderNum.size() != 6 && orderNum.size() != 8 && orderNum.size() != 9){
+        QMessageBox::critical(this,tr("Error"),tr("伝票番号は６桁,８桁,９桁です。"));
+        return;
     }
 
-    //ディレクトリ開く
-    QDesktopServices::openUrl( QUrl::fromLocalFile ( path ));
+
+    bool isLegacyOrderType = orderNum.size() == 6;//最後が６桁の時は昔の伝票番号形式
+
 
     //txtEditのクリア
     ui->orderNumberEdit->clear();
+
+
+    if(isLegacyOrderType){
+        int fromYear = 2012;
+        QDir rootDir(root);
+
+        foreach(QString yearDirName , rootDir.entryList(QDir::Filter::AllDirs, QDir::SortFlag::Name)){
+            bool isDigit;
+            if (yearDirName.toInt(&isDigit) > fromYear){
+                continue;
+            }
+            if(!isDigit) continue;
+
+            QDir yearDir(root + '/' + yearDirName);
+            foreach(QString monthDirName , yearDir.entryList(QDir::Filter::AllDirs, QDir::SortFlag::Name)){
+                monthDirName.toInt(&isDigit);
+                if(!isDigit) continue;
+
+                QString orderDir = yearDirName + '/' + monthDirName + '/' + orderNum;
+                QString searchPath = root + '/' + orderDir;
+                QDir searchDir(searchPath);
+                qDebug() << searchPath;
+                if(searchDir.exists()){
+                    //ディレクトリ開く
+                    QDesktopServices::openUrl( QUrl::fromLocalFile ( searchPath ));
+                    return;
+                }
+            }
+        }
+        //捜査しても見つからない
+        QMessageBox::warning(this,tr("Notice"),tr("伝票フォルダが見つかりません"));
+        return;
+
+    }else{//通常伝票
+        QString orderDir = "20" + orderNum.left(2) + "/" + orderNum.mid(2, 2) + "/" + orderNum.right(orderNum.length() - 4);
+        QString searchPath = root + '/' + orderDir;
+        //存在しない場合は作成
+        QDir searchDir(searchPath);
+        if (!searchDir.exists()){
+            if(!ui->createDirectoryCheck->isChecked()){//新規作成しないモードon
+                QMessageBox::warning(this,tr("Notice"),tr("伝票フォルダが見つかりません"));
+                return;
+            }
+            searchDir.mkpath(".");
+        }
+        //ディレクトリ開く
+        QDesktopServices::openUrl( QUrl::fromLocalFile ( searchPath ));
+        return;
+    }
+
+
+
 }
